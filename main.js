695c69752f1a556a4449c4aceb41c5182fe0140f
William Franklin <emailwilliamfranklin@gmail.com>
2024-08-16 14:37:08 -0700

    Init copy from InteractiveDesignModels

---
+++ main.js
@@ -0,0 +1,10 @@
+import { FloatingTab, Taskbar } from "./src/ui/Containers";
+
+export function UIElements() {
+  let elements = {};
+  //elements.fb = FloatingTab();
+  elements.tb = Taskbar();
+
+  console.log(elements);
+  return elements;
+}
